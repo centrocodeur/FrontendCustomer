687d0471e36bd4cc47a806d020c25bcb91fa8221
centrocodeur <centrocodeur@gmail.com>
2025-04-10 22:41:32 +0200

    Update backend url

--- src/app/servicies/auth/auth.service.ts
+++ src/app/servicies/auth/auth.service.ts
@@ -4,7 +4,7 @@ import {UserStorageService} from "../storage/user-storage.service";
 import {map, Observable} from "rxjs";
 
 //const BASIC_URL= "http://localhost:8080/";
-const BASIC_URL= "https://studiprojetbackend-production.up.railway.app/";
+const BASIC_URL= "https://studi-jo-backend-production.up.railway.app/";
 @Injectable({
   providedIn: 'root'
 })
